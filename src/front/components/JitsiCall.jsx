65c1fa107a909b92e3f99286986e1034af09ab71
lexsaso <jalexanderssoto@hotmail.com>
2026-09-14 09:09:01 +0000

    jitsicall

--- src/front/components/JitsiCall.jsx
+++ src/front/components/JitsiCall.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { JitsiMeeting } from "@jitsi/react-sdk";
 
 const JitsiCall = () => {
-    return (
-        <JitsiMeeting
-            domain="meet.jit.si"
-            roomName="teleconsulta-prueba-123"
-            userInfo={{
-                displayName: "Usuario de prueba"
-            }}
-            getIFrameRef={(iframeRef) => {
-                iframeRef.style.height = "100%";
-                iframeRef.style.width = "100%";
-            }}
-        />
-    );
+  return (
+    <div style={{ height: "600px", width: "100%" }}>
+      <JitsiMeeting
+        domain="meet.jit.si"
+        roomName="teleconsulta-prueba-123"
+        userInfo={{
+          displayName: "Usuario de prueba",
+        }}
+        getIFrameRef={(iframeRef) => {
+          iframeRef.style.height = "100%";
+          iframeRef.style.width = "100%";
+        }}
+      />
+    </div>
+  );
 };
 
 export default JitsiCall;
